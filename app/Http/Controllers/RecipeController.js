10eaa653a87c12db6355168f79bc9440f22bf816
Horváth Győző <gyozke@inf.elte.hu>
2016-10-17 13:46:08 +0200

    6. gyak

--- app/Http/Controllers/RecipeController.js
+++ app/Http/Controllers/RecipeController.js
@@ -3,6 +3,7 @@
 const Database = use('Database')
 const Category = use('App/Model/Category')
 const Recipe = use('App/Model/Recipe')
+const Validator = use('Validator')
 
 class RecipeController {
 
@@ -21,6 +22,51 @@ class RecipeController {
       name: 'Győző',
       categories: categories.toJSON()
     })  
+  }
+
+  * create (request, response) {
+    const categories = yield Category.all()
+    yield response.sendView('recipeCreate', {
+      categories: categories.toJSON()
+    });
+  }
+
+  * doCreate (request, response) {
+    const recipeData = request.except('_csrf');
+
+    const rules = {
+      name: 'required',
+      ingredients: 'required',
+      instructions: 'required',
+      category_id: 'required'
+    };
+
+    const validation = yield Validator.validateAll(recipeData, rules)
+
+    if (validation.fails()) {
+      yield request
+        .withAll()
+        .andWith({errors: validation.messages()})
+        .flash()
+      response.redirect('back')
+      return
+    }
+
+    recipeData.user_id = 1
+    const recipe = yield Recipe.create(recipeData)
+    // response.send(recipe.toJSON())
+    response.redirect('/')
+  }
+
+  * show (request, response) {
+    const id = request.param('id');
+    const recipe = yield Recipe.find(id);
+    yield recipe.related('category').load();
+    // response.send(recipe.toJSON())
+
+    yield response.sendView('recipeShow', {
+      recipe: recipe.toJSON()
+    })
   }
 
 }
