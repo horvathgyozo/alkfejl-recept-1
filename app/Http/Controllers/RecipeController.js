5b9ec8f580604545fd706e6ef4d2ffef30f19934
Horváth Győző <gyozke@inf.elte.hu>
2016-11-07 13:56:27 +0100

    8. gyak

--- app/Http/Controllers/RecipeController.js
+++ app/Http/Controllers/RecipeController.js
@@ -58,6 +58,60 @@ class RecipeController {
     response.redirect('/')
   }
 
+  * edit (request, response) {
+    const categories = yield Category.all()
+    const id = request.param('id');
+    const recipe = yield Recipe.find(id);
+    console.log(recipe.toJSON())
+
+    if (request.currentUser.id !== recipe.user_id) {
+      response.unauthorized('Access denied.')
+      return
+    }
+
+
+    yield response.sendView('recipeEdit', {
+      categories: categories.toJSON(),
+      recipe: recipe.toJSON()
+    });
+  }
+
+  * doEdit (request, response) {
+    const recipeData = request.except('_csrf');
+
+    const rules = {
+      name: 'required',
+      ingredients: 'required',
+      instructions: 'required',
+      category_id: 'required'
+    };
+
+    const validation = yield Validator.validateAll(recipeData, rules)
+
+    if (validation.fails()) {
+      yield request
+        .withAll()
+        .andWith({errors: validation.messages()})
+        .flash()
+      response.redirect('back')
+      return
+    }
+
+    const id = request.param('id');
+    const recipe = yield Recipe.find(id);
+
+    // Object.assign(recipe, recipeData)
+    
+    recipe.name = recipeData.name;
+    recipe.ingredients = recipeData.ingredients; 
+    recipe.instructions = recipeData.instructions;
+    recipe.category_id = recipeData.category_id;
+
+    yield recipe.save()
+    
+    response.redirect('/')
+  }
+
   * show (request, response) {
     const id = request.param('id');
     const recipe = yield Recipe.find(id);
@@ -65,8 +119,16 @@ class RecipeController {
     // response.send(recipe.toJSON())
 
     yield response.sendView('recipeShow', {
-      recipe: recipe.toJSON()
+      recipe: recipe.toJSON(),
+      user: request.currentUser
     })
+  }
+
+  * doDelete (request, response) {
+    const id = request.param('id');
+    const recipe = yield Recipe.find(id);
+    yield recipe.delete()
+    response.redirect('/')
   }
 
 }
